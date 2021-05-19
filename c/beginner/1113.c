fe945739e51a5aafcff315ee03d4da9fa62e8df2
Aran Leite <hyoretsu@gmail.com>
2021-05-19 11:03:23 -0300

    Solved #1113 (C)

---
+++ c/beginner/1113.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+ int x = 0, y = 1;
+
+ while (x != y){
+  scanf("%d %d", &x, &y);
+
+  if (x < y) {
+   printf("Crescente\n");
+  } else if (x > y) {
+   printf("Decrescente\n");
+  }
+ }
+
+ return 0;
+}
